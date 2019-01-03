2de708348242a5caef2470c35c8e7fc9040b3459
许长安 <cax_dmu@163.com>
2019-01-03 23:20:44 +0800

    add eureka

---
+++ spring-cloud-eureka-order-client/src/main/java/com/xuca/study/OrderApplication.java
@@ -0,0 +1,18 @@
+package com.xuca.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableDiscoveryClient
+@RestController
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
